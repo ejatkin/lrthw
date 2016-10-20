9470b366477916d559b27e61d53a2bbb5f4c59aa
ejatkin <ejatkin@gmail.com>
2016-10-20 15:14:52 +0000

    Seventh commit - lrthw exercise 07

---
+++ ex7.rb
@@ -0,0 +1,28 @@
+# Each of the three lines below outputs to the screen the string of text
+# within the quote marks
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+#This will output to screen 10 dots in a line
+puts "." * 10 # what'd that do?
+
+# Each of the 12 lines below creates a new variable which is then assigned
+# a single letter as its string
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# The print allows Cheese to be printed on the same line; using puts would 
+# mean it was printed on a different line
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
