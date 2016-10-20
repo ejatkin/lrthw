2078b30bb26a0e3f83c2adedf8a0efcece0c1b04
ejatkin <ejatkin@gmail.com>
2016-10-20 16:25:14 +0000

    Eleventh commit - lrthw exercise 11

---
+++ ex11.rb
@@ -0,0 +1,15 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+print "What colour eyes do you have? "
+eyes = gets.chomp
+print "What's your favourite film? "
+film = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+puts "You have #{eyes} eyes and your favourite film is #{film}."
+
+# gets.chomp removes the line break at the end of the user's answer
