dbaa6c082dae63301d95f8fe1e4cb4dacbe555a8
ejatkin <ejatkin@gmail.com>
2016-10-20 14:24:11 +0000

    Fifth commit - lrthw exercise 05

---
+++ ex5.rb
@@ -0,0 +1,23 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+height_cm = height * 2.54
+weight_kg = weight / 2.2
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+# adding new line to check my new variables
+puts "My height in inches is #{height} and in cm is #{height_cm}."
+puts "My weight in pounds is #{weight} and in kg is #{weight_kg}."
