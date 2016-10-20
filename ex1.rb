a04c607cf8df7b85a3305b57b02132754464d15f
ejatkin <ejatkin@gmail.com>
2016-10-20 10:43:27 +0000

    First commit - lrthw exercise 01

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+#puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+#puts "This is fun."
+puts "Yay! Printing."
+#puts "I'd much rather you 'not'."
+#puts 'I "said" do not touch this.'
+#puts "I am making my script run another line."
+#The octothorpe character adds a comment i.e. the line will not be printed
