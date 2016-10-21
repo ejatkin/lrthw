63a5402836a26379a99b77aa9a9d96c02cc5eb90
ejatkin <ejatkin@gmail.com>
2016-10-21 16:27:26 +0000

    Twenty ninth commit - lrthw exercise 29

---
+++ ex29.rb
@@ -0,0 +1,40 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats
+puts "Too many cats! The world is doomed!"
+end
+
+if people > cats
+puts "Not many cats! The world is saved!"
+end
+
+if people < dogs
+puts "The world is drooled on!"
+end
+
+if people > dogs
+puts "The world is dry!"
+end
+
+
+dogs += 5
+
+if people >= dogs
+puts "People are greater than or equal to dogs."
+end
+
+if people <= dogs
+puts "People are less than or equal to dogs."
+end
+
+
+if people == dogs
+puts "People are dogs."
+end
+
+# Answer to Q1 The if is asking a question essentially asking "Is this true?"
+# if the expression evaluates to true then it's printed, but if it's false
+# then it's not
