66e7a7994b67d59b41609b5d78382b886b5ed3a6
ejatkin <ejatkin@gmail.com>
2016-10-22 12:18:26 +0000

    Thirty fourth commit - lrthw exercise 34

---
+++ ex34.rb
@@ -0,0 +1,6 @@
+my_books = ['The Snow Child', 'Harry Potter', 'His Dark Materials', 'Cider with Rosie']
+
+puts "The first book is #{my_books[0]}"
+puts "The second book is #{my_books[1]}"
+puts "The third book is #{my_books[2]}"
+puts "The fourth book is #{my_books[3]}"
